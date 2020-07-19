754402947e7069f05afbb12edfe0eab3476191da
LAWAL ABD'KEHINDE <32939546+kennie-larkson@users.noreply.github.com>
2020-07-19 06:10:27 +0100

    React App

---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const year = new Date().getFullYear();
+function Footer() {
+    return(
+        <div>
+            <footer>
+                <p>Copyright{year}</p>
+            </footer>
+        </div>
+        
+    );
+};
+
+export default Footer;
